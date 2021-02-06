9ec6fba826e2b6743d9f193bfca74feecab29715
Arthur CS <arthur.cs147@gmail.com>
2021-02-05 23:33:56 -0300

    Created the HTML5 structure and created the header tag with a h1, input:checkbox and a nav tag with three li's

---
+++ View/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/navbar.css">
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
+    <script src="./scripts/main.js"></script>
+    <title>Home | Acs_Code</title>
+</head>
+<body>
+    <header>
+        <div class="main-header">
+            <h1>Acs_Code</h1>
+            <input type="checkbox" class="btn-navbar">
+        </div>
+
+        <nav class="navBar">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Courses</a></li>
+                <li><a href="#">Contact</a></li>
+
+                <li><a href="#">Log in</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
